5bda0929c397d6e1d7ce2a294f1840ba749a9bc8
DoubleBird <214778293@qq.com>
2018-02-08 13:26:09 +0800

    ...

--- app.js
+++ app.js
@@ -18,12 +18,6 @@ app.use(require('./router/info'));
 // app.use(function (req, res, next) {
 //     res.status(404).send('404 NOT FOUND !!!');
 // });
+var serverPort = process.env.PORT || 5000;
 
-var server = app.listen(process.env.PORT, function () {   
-
-    var host = server.address().address;
-    var port = server.address().port;
-
-    console.log('Example app listening at http://%s:%s', host, port);
-
-});    
+var server = app.listen(serverPort);    
